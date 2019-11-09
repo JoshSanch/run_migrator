216601d6a92005aea4d4f40b6844d30bab88bb83
Josh Sanchez <joshuaesanch@gmail.com>
2019-11-09 14:18:53 -0600

    Update main func to generate dump file of runs

--- run_mover.py
+++ run_mover.py
@@ -1,5 +1,5 @@
 import credentials_loader
-from migration_utils import get_target_runs, post_runs
+from migration_utils import get_target_runs, generate_run_request_data, dump_runs
 import game_ids
 
 def move_runs(source_category_id, target_category_id, api_key_path):
@@ -10,12 +10,16 @@ def move_runs(source_category_id, target_category_id, api_key_path):
         existing_runs = get_target_runs(source_category_id)
 
         # Start posting them to a new board
-        post_runs(existing_runs)
+        #post_runs(existing_runs)
         
     except Exception as ex:
         print("Error migrating runs. The following exception occurred:")
         print(ex.with_traceback)
 
 if __name__ == "__main__":
-    runs = get_target_runs(game_ids.C_TSSM_CHEATPERCENT)
-    print(runs[0])
+    source_id = game_ids.C_TSSM_CHEATPERCENT
+    target_id = game_ids.C_TSSMCE_CHEATPERCENT
+
+    runs = get_target_runs(source_id)
+    primed_runs = generate_run_request_data(runs, target_id)
+    dump_runs(primed_runs, "test.txt")
