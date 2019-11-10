017e924fc90e72c809c36d8cbb21a38fca8b1bea
Josh Sanchez <joshuaesanch@gmail.com>
2019-11-09 23:17:47 -0600

    Update main method for run_mover

--- run_mover.py
+++ run_mover.py
@@ -1,17 +1,43 @@
 import credentials_loader
-from migration_utils import get_target_runs, generate_run_request_data, dump_runs, post_formatted_runs, RUNS_ENDPOINT
+from migration_utils import retrieve_target_runs, generate_run_request_data, dump_runs, post_formatted_runs, RUNS_ENDPOINT
 import game_ids
+
+"""
+SRC has had a long running issue (documented as early as March of 2019) where
+submitting runs on behalf of other players - a 500 response occurs every
+time this request is made. To work around this, we make a mod note on each run that
+states what the submitter's SRC username (or the guest name, if applicable).
+Then, moderators will need to go in and manually copy-paste this value into the runner
+field to update to the correct value.
+
+Check on the status of this issue at:
+https://github.com/speedruncomorg/api/issues/87
+"""
+WORKAROUND_ACTIVE = True
 
 def move_runs(source_category_id, target_category_id, api_key_path):
     try:
         api_key = credentials_loader.get_api_key(api_key_path)
 
         # Get runs and copy them to be POSTed to new location
-        existing_runs = get_target_runs(source_category_id)
-        primed_runs = generate_run_request_data(existing_runs, target_id)
+        print("API key loaded successfully.")
+        print("Getting source leaderboard runs from Speedrun.com ...\n")
+
+        current_board_runs = retrieve_target_runs(source_category_id)
+
+        print("Runs successfully acquired.")
+        print("Generating new run submissions based on existing runs ...\n")
+
+        generated_submissions = generate_run_request_data(current_board_runs, target_id, WORKAROUND_ACTIVE)
+
+        print("Run submissions generated.")
 
         # Start posting them to a new board
-        post_formatted_runs(primed_runs, RUNS_ENDPOINT, api_key)
+        print("Posting generated submissions ...\n")
+
+        post_formatted_runs(generated_submissions, RUNS_ENDPOINT, api_key)
+
+        print("Runs successfully posted. Leaderboard copying complete.")
         
     except Exception as ex:
         print("Error migrating runs. The following exception occurred:")
