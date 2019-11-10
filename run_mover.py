fd2ab26abf0599bd99174b520e5aa79e39cc4053
Joshua Sanchez <joshuaesanch@gmail.com>
2019-11-09 19:32:14 -0600

    Update move_runs method with latest and greatest utils

--- run_mover.py
+++ run_mover.py
@@ -1,5 +1,5 @@
 import credentials_loader
-from migration_utils import get_target_runs, generate_run_request_data, dump_runs
+from migration_utils import get_target_runs, generate_run_request_data, dump_runs, post_formatted_runs, RUNS_ENDPOINT
 import game_ids
 
 def move_runs(source_category_id, target_category_id, api_key_path):
@@ -8,18 +8,18 @@ def move_runs(source_category_id, target_category_id, api_key_path):
 
         # Get runs and copy them to be POSTed to new location
         existing_runs = get_target_runs(source_category_id)
+        primed_runs = generate_run_request_data(existing_runs, target_id)
 
         # Start posting them to a new board
-        #post_runs(existing_runs)
+        post_formatted_runs(primed_runs, RUNS_ENDPOINT, api_key)
         
     except Exception as ex:
         print("Error migrating runs. The following exception occurred:")
-        print(ex.with_traceback)
+        print(ex)
 
 if __name__ == "__main__":
     source_id = game_ids.C_TSSM_CHEATPERCENT
     target_id = game_ids.C_TSSMCE_CHEATPERCENT
 
-    runs = get_target_runs(source_id)
-    primed_runs = generate_run_request_data(runs, target_id)
-    dump_runs(primed_runs, "test.txt")
+    move_runs(source_id, target_id, "api_key.json")
+    
