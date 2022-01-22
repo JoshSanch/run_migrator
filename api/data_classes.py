8de6d58395e5dde40836de21993da4e595c0d714
Josh Sanchez <joshuaesanch@gmail.com>
2022-01-22 14:33:14 -0600

    Fix dataclass errors relating to default fields

--- api/data_classes.py
+++ api/data_classes.py
@@ -5,16 +5,17 @@ class RunData:
     id: str
     weblink: str
     game: str
-    level: str = None
-    category: str
-    videos: dict
-    comment: str
-    status: dict
-    players: list
-    date: str = None
-    submitted: str = None
     times: dict
     system: dict
     splits: dict
     values: dict
     links: list
+    category: str
+    videos: dict
+    comment: str
+    status: dict
+    players: list
+    level: str = None
+    date: str = None
+    submitted: str = None
+    
