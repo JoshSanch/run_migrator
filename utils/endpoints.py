cc27587cc88299c57054aabe32833f5f5d426eee
Josh Sanchez <joshuaesanch@gmail.com>
2022-01-24 01:07:36 -0600

    Add endpoints for games and levels

--- utils/endpoints.py
+++ utils/endpoints.py
@@ -1,2 +1,4 @@
 RUNS_ENDPOINT = "https://www.speedrun.com/api/v1/runs"
 CATEGORIES_ENDPOINT = "https://www.speedrun.com/api/v1/categories"
+GAME_ENDPOINT = "https://www.speedrun.com/api/v1/game"
+LEVELS_ENDPOINT = "https://www.speedrun.com/api/v1/level"
